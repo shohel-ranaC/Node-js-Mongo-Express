7f8ea13cc325da295c89bab0cb316ffa5905bf72
shohel-ranaC <akashshohel422@gmail.com>
2022-10-30 20:32:25 -0700

    express env file setup

---
+++ express-setup-environment-4/index.js
@@ -0,0 +1,13 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+
+ app.get('/', (req, res) => {  
+    res.send('hello world');
+});
+
+app.listen(PORT, ()=>{
+    console.log(`Server is running at port http://localhost:${PORT}`);
+});
